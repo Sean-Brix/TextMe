f1c36d7542a524986611b542e94e5446bcb8572e
Kc Sean Brix Calinao <kcseancalinao@gmail.com>
2025-04-20 21:10:08 +0800

    v1.1.0 - Fully setup request handler, handling initial router.

---
+++ server/app.js
@@ -0,0 +1,48 @@
+import cookieParser from 'cookie-parser';
+import express, { urlencoded } from 'express';
+import session from 'express-session';
+import store from 'connect-mongo';
+import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Configuration
+dotenv.config();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const DB_URI = process.env.MONGO_URI;
+const SKEY = process.env.SESSION_KEY || '----';
+
+// Request Handler
+const app = express();
+
+app.use(urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+    session({
+        store: store.create({
+            mongoUrl: DB_URI,
+            collectionName: 'sessions',
+        }),
+
+        secret: SKEY,
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+            maxAge: 1000 * 60 * 60 * 24, // 24 hours
+            httpOnly: true,
+        },
+    })
+);
+
+// Request Routes
+import index_rt from './Routes/index_rt.js';
+app.use('/', index_rt);
+
+// Invalid Route
+app.use((req, res) => {
+    res.status(404).send('<h1>404 Page Not Found</h1>');
+});
+
+export default app;
---
+++ server/server.js
@@ -0,0 +1,28 @@
+import http from 'http';
+import app from './app.js';
+import dotenv from 'dotenv';
+import colors from 'colors';
+import mongoose from 'mongoose';
+
+// Configuration
+dotenv.config();
+colors.enable();
+const PORT = process.env.PORT || 3000;
+const DB_URI = process.env.MONGO_URI;
+
+// Database
+mongoose.connect(DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+// Server
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+    console.log(
+        '\n\n\n\nLINK: '.cyan + ('http://127.0.0.1:' + PORT + '/\n\n\n').yellow.italic.underline
+    );
+});
+
+export default server;
