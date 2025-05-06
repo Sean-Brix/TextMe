fa7f820e5c3b38a5a34adadc4abb54c8c407c93d
Kc Sean Brix Calinao <kcseancalinao@gmail.com>
2025-05-06 14:18:31 +0800

    v1.16.1 - Made multiple tickets before leaving for my school projects

--- client/src/Pages/People/Sub/Profile.jsx
+++ client/src/Pages/People/Sub/Profile.jsx
@@ -3,9 +3,18 @@ import style from './Profile.module.css';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
+
+/* 
+    TODO: The first one is to make sure the current account isnt displayed on the people page and message page
+    TODO: Add a section for looking all friend request sent to the current user
+    TODO: Add a section to view all your current request
+    TODO: Better navigation
+
+*/ 
+
+
+
 export default function Profile({ _id, username, email, profilePicture, friend_list, createdAt,}) {
-    // TODO: Complete all request state, remove users own account on the list to avoid self request
-    // TODO: Make sure to add req_Exist function for every control so no duplication of request
 
     const [isFriend, setIsFriend] = useState(null);
     const navigate = useNavigate();
